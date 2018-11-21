74b761ea74f3e9abc3f6e13e06552d534625c679
Zlash65 <zarrar65@gmail.com>
2018-11-21 22:02:08 +0530

    separate classes into different files unique word count logic added

---
+++ streamer_listener.py
@@ -0,0 +1,137 @@
+from tweepy.streaming import StreamListener
+import json, re, tldextract, string
+from nltk.corpus import stopwords
+from collections import Counter
+
+# Python 2 and 3 support
+try:
+	import urllib2 as liburl
+except ModuleNotFoundError:
+	import urllib.request as liburl
+
+class StreamerListener(StreamListener):
+
+	def __init__(self, db):
+		''' capture connection to mongo '''
+		self.db = db
+
+	def on_data(self, data):
+		''' each tweet calls this method where data contains the information about the tweet '''
+
+		data = json.loads(data)
+
+		self.store_user_report_data(data)
+		self.parse_links_report_data(data)
+		self.parse_words(data)
+
+		return True
+
+	def on_error(self, status):
+		print(status)
+		return False
+
+	def store_user_report_data(self, data):
+		'''
+			Extract User's twitter handle and their tweet count from
+			incoming streaming data.
+		'''
+
+		exists = self.db.tweet_count.find_one({
+			'user': data['user']['screen_name']
+		})
+
+		if exists:
+			self.db.tweet_count.update_one(
+				{'tweets': data['user']['screen_name']},
+				{'$set': {'tweets': data['user']['statuses_count']}},
+				upsert=False
+			)
+		else:
+			self.db.tweet_count.insert({
+				'user': data['user']['screen_name'],
+				'tweets': data['user']['statuses_count']
+			})
+
+	def parse_links_report_data(self, data):
+		'''
+			Parse the tweet and use regex to find link from it.
+			Open the url to fetch its actual url as links can be shortened.
+			Insert links that failed to be resolved maybe because at the time of tweeting
+			the characters constraint truncated the link or etc.
+		'''
+
+		# extract the actual tweet
+		text = data["text"].encode("utf-8")
+
+		# find url from the tweet data
+		urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+
+		for url in urls:
+
+			try:
+				req = liburl.Request(url, headers={'User-Agent' : "Magic Browser"})
+				original_url = liburl.urlopen(req).geturl()
+				extract = tldextract.extract(original_url)
+				self.store_links_report_data(original_url, extract)
+			except:
+				original_url = url
+				self.db.failed_urls.insert({'url': url})
+
+	def store_links_report_data(self, original_url, extract):
+		'''
+			Store the domain of the url.
+			Also update the count of domain if it already exists.
+		'''
+
+		exists = self.db.link_url.find_one({
+			'domain': extract.domain
+		})
+
+		if exists:
+			self.db.link_url.update_one(
+				{'domain': extract.domain},
+				{'$set': {'count': exists["count"]+1}},
+				upsert=False
+			)
+		else:
+			self.db.link_url.insert({
+				'domain': extract.domain,
+				'count': 1
+			})
+
+	def parse_words(self, data):
+		'''
+			Remove punctuation from text.
+			Use Natural Language toolkit (nltk) to filter out common words.
+			Use Counter function to find unique words and their count in the words list.
+		'''
+
+		text = data["text"].encode("utf-8")
+		out = text.translate(string.maketrans("",""), string.punctuation)
+
+		s = set(stopwords.words('english'))
+
+		words = filter(lambda w: not w in s,out.split())
+		words_list = Counter(words)
+
+		for row in words_list:
+			self.store_words(row, words_list[row])
+
+	def store_words(self, word, count):
+		''' store words and their count in database '''
+
+		exists = self.db.words.find_one({
+			'word': word
+		})
+
+		if exists:
+			self.db.words.update_one(
+				{'word': word},
+				{'$set': {'count': exists["count"] + count}},
+				upsert=False
+			)
+		else:
+			self.db.words.insert({
+				'word': word,
+				'count': count
+			})
---
+++ twitter_streamer.py
@@ -0,0 +1,116 @@
+from tweepy import OAuthHandler, Stream
+from streamer_listener import StreamerListener
+from pymongo import MongoClient
+from prettytable import PrettyTable
+
+import twitter_credentials as creds, time, pymongo
+
+
+class TwitterStreamer():
+	"""
+		Establish connection with database
+		Establish conncetion with Stream API
+		Print generated reports every minute
+	"""
+
+	def __init__(self):
+		''' set up connection variables and clear previos data from db tables '''
+
+		self.auth = OAuthHandler(creds.CONSUMER_KEY, creds.CONSUMER_KEY_SECRET)
+		self.auth.set_access_token(creds.ACCESS_TOKEN, creds.ACCESS_TOKEN_SECRET)
+
+		self.connection = MongoClient(creds.DB_HOST, creds.DB_PORT)
+		self.db = self.connection[creds.DB_NAME]
+		self.db.authenticate(creds.DB_USER, creds.DB_PASS)
+		self.clear_tables()
+
+	def stream_tweets(self, keywords):
+		'''
+			Start streaming data asynchronously.
+			Print reports every 1 minute
+			Press CTRL+C to terminate stream 
+		'''
+
+		listener = StreamerListener(self.db)
+
+		stream = Stream(self.auth, listener)
+
+		stream.filter(track=keywords, async=True)
+
+		minute = 1
+		try:
+			while(True):
+				time.sleep(60)
+				self.print_data(minute)
+				minute += 1
+		except KeyboardInterrupt:
+			print("Terminating Stream")
+			stream.disconnect()
+
+	def clear_tables(self):
+		''' Clears out data from tables '''
+		self.db.tweet_count.remove({})
+		self.db.link_url.remove({})
+		self.db.failed_urls.remove({})
+		self.db.words.remove({})
+
+	def print_data(self, minute):
+		print("Data since {0} minutes".format(minute))
+
+		self.print_user_data()
+		print("-"*50)
+		self.print_link_data()
+		print("-"*50)
+		self.print_words_data()
+		print("-"*50)
+
+	def print_user_data(self):
+		''' Prints User and their Tweet count '''
+
+		user_report = PrettyTable(['User', "Tweets"])
+
+		for row in self.db.tweet_count.find():
+			user_report.add_row([row["user"], row["tweets"]])
+
+		print(user_report)
+
+	def print_link_data(self):
+		'''
+			Prints total links received from incoming streaming data [ working as well as failed ]
+			Print total number of links that couldn't be resolved.
+			Prints a table displaying domain of the links with their count.
+		'''
+
+		link_report = PrettyTable(['Domain', "Count"])
+		failed_urls = self.db.failed_urls.find()
+		link_count, failed_count = 0, failed_urls.count()
+
+		for row in self.db.link_url.find().sort('count', pymongo.DESCENDING):
+			link_count += row["count"]
+			link_report.add_row([row["domain"], row["count"]])
+
+		print("Total links = " + str(failed_count + link_count))
+		print("Failed to resolve links = " + str(failed_urls.count()))
+		print(link_report)
+
+	def print_words_data(self):
+		''' Prints table containing unique words and their count '''
+
+		words_report = PrettyTable(['Words', "Count"])
+
+		for row in self.db.words.find().sort('count', pymongo.DESCENDING).limit(10):
+			words_report.add_row([row["word"], row["count"]])
+
+		print(words_report)
+
+if __name__ == "__main__":
+
+	streamer = TwitterStreamer()
+
+	# python 2 and 3 supported
+	try:
+		keyword = raw_input("Please enter the keyword you want to track : ")
+	except NameError:
+		keyword = input("Please enter the keyword you want to track : ")
+
+	streamer.stream_tweets([keyword])
